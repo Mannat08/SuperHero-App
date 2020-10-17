f21d2f2551eb7e92609c53769e3faf7906964d61
Mannat08 <72085281+Mannat08@users.noreply.github.com>
2020-10-17 23:23:06 +0530

    Add files via upload

---
+++ build/precache-manifest.52550773e2ef0347b7ceeafdc2902259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c1d5903ecf2d1f6002efc018ad63c24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0785564c142eb2e12fbe",
+    "url": "/static/css/main.d02b25ce.chunk.css"
+  },
+  {
+    "revision": "b98b827165278c8cc102",
+    "url": "/static/js/2.73c02fc1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.73c02fc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0785564c142eb2e12fbe",
+    "url": "/static/js/main.bc66769d.chunk.js"
+  },
+  {
+    "revision": "22b81406c7848a4c5f58",
+    "url": "/static/js/runtime-main.1c206231.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
